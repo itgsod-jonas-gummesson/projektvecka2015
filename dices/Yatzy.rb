debd37c06b364f088f16e5cc7e751b093019676d
itgsod-carl-alexandersson <carl.alexandersson@elev.it-gymnasiet.se>
2015-03-24 14:42:07 +0100

    Ability to rulla två times now existerar. Men more is soon att komma.

--- dices/Yatzy.rb
+++ dices/Yatzy.rb
@@ -38,7 +38,7 @@ def first_keep
     first_keep
   elsif $keep == 'five'
     $keep5=1
-    puts "Dice #{keep} is being kept."
+    puts "Dice #{$keep} is being kept."
     first_keep
   elsif $keep == 'roll'
 
@@ -52,21 +52,72 @@ puts 'Rolling the non kept dice.'
 if $keep1 ==0
   $dice1=rand(1..6)
   puts "Dice one rolled #{$dice1}"
-  end
+end
+if $keep1 ==1
+  puts "Dice one is still #{$dice1}"
+end
 if $keep2 ==0
   $dice2=rand(1..6)
   puts "Dice two rolled #{$dice2}"
-  end
+end
+if $keep2 ==1
+puts "Dice two is still #{$dice2}"
+end
 if $keep3 ==0
   $dice3=rand(1..6)
   puts "Dice three rolled #{$dice3}"
-  end
+end
+if $keep3 ==1
+  puts "Dice three is still #{$dice3}"
+end
 if $keep4 ==0
   $dice4=rand(1..6)
   puts "Dice four rolled #{$dice4}"
-  end
+end
+if $keep4 ==1
+  puts "Dice four is still #{$dice4}"
+end
 if $keep5 ==0
   $dice5=rand(1..6)
   puts "Dice five rolled #{$dice5}"
+end
+if $keep5 ==1
+  puts "Dice five is still #{$dice5}"
+end
 
+
+
+
+
+def second_keep
+  puts 'Which dice would you like to keep? Type the number of the dice to keep it, type roll to roll again. (one / two / three / four / five / roll)'
+  $keep=gets.chomp
+
+
+  if $keep == 'one'
+    $keep1=1
+    puts "Dice #{$keep} is being kept."
+    second_keep
+  elsif $keep == 'two'
+    $keep2=1
+    puts "Dice #{$keep} is being kept."
+    second_keep
+  elsif $keep == 'three'
+    $keep3=1
+    puts "Dice #{$keep} is being kept."
+    second_keep
+  elsif $keep == 'four'
+    $keep4=1
+    puts "Dice #{$keep} is being kept."
+    second_keep
+  elsif $keep == 'five'
+    $keep5=1
+    puts "Dice #{$keep} is being kept."
+    second_keep
+  elsif $keep == 'roll'
+
+  else
+    puts 'That is not a valid input...'
+    second_keep
+  end
 end
